b81e4dd28b371519331e77dc445db08cda73f328
ichibanmikan <1278074877@qq.com>
2021-03-24 19:35:01 +0800

    update

--- class2.c
+++ class2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main() {
     int hundred, ten, ind, i, ans;
-    for(i = 100; i < 1000; i++) {
-        hundred = i / 100;
+    for(i=152; i < 154; i++) {
+        hundred = i/100;
         ten = (i - hundred * 100) / 10;
         ind = i % 10;
         
-        if(i == hundred*hundred*hundred + ten*ten*ten + ind*ind*ind)
+        if(i==hundred*hundred*hundred + ten*ten*ten + ind*ind*ind)
             ans = i;
     }
     return 0;
