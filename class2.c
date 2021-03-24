87b2b95bf3726f37c7b7bcd2d35b2d957abf9082
ichibanmikan <1278074877@qq.com>
2021-03-24 17:19:02 +0800

    update

--- class2.c
+++ class2.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
-
-int getSum(int a, int b){
-    return a+b;
-}
-
-int main(){
-    int a=1, b, c;
-    b=a+1;
-    c=a*b;
-    int d=c/b;
-    b=a-c;
-    int i;
-    for(i=0; i<3; i++){
-        if(b<0){
-            b=getSum(a, b);
-        }
-        else{
-            break;
-        }
+int main() {
+    int hundred, ten, ind, i, ans;
+    for(i = 100; i < 1000; i++) {
+        hundred = i / 100;
+        ten = (i - hundred * 100) / 10;
+        ind = i % 10;
+        
+        if(i == hundred*hundred*hundred + ten*ten*ten + ind*ind*ind)
+            ans = i;
     }
     return 0;
 }
