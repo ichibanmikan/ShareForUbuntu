b261142398909338295166b6c11f72a15458aa59
ichibanmikan <1278074877@qq.com>
2021-04-14 20:50:15 +0800

    计算机系统datalab实验

--- Coex/LAB2-datalab/datalab-handout/bits.c
+++ Coex/LAB2-datalab/datalab-handout/bits.c
@@ -256,8 +256,9 @@ int isLessOrEqual(int x, int y) {
   int tempx=x>>31;
   int tempy=y>>31;
   int a=0x0;
-  a=(~a+(~1+1));;
-  return ((~tempy|tempx)&1)|(!((!!(x+(~y+1)))&(~(a|((x+(~y+1))>>31))))); //y取反和x相或并且按位与1，xy正负不同时满足得1不满足得0。同时按照isPositive函数的方法并取反，x-y(x+(~y+1))小于0则得
+  a=(~a+(~1+1));
+  // return ((~tempy|tempx)&1)|(!((!!(x+(~y+1)))&(~(a|((x+(~y+1))>>31))))); //y取反和x相或并且按位与1，xy正负不同时满足得1不满足得0。同时按照isPositive函数的方法并取反，x-y(x+(~y+1))小于0则得
+  return ((!(x+(~y+1))|((x+(~y+1)>>31));
 }
 /*
  * ilog2 - return floor(log base 2 of x), where x > 0
